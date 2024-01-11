62ab48f6b567521572a139b473c917dad2d2e26d
El Ghalbzouri Adnan <elghalbzouriadnan@gmail.com>
2024-01-11 16:05:57 +0100

    mises en place de la page list employee

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -1,5 +1,17 @@
-const Table = () => {
-  return <div></div>;
-};
+import DataTable from "@ognimelo/hrnet-datatable";
+import "./Table.scss";
+import { dataColumn } from "../../data/MockUpListEmployees.json";
+// import { dataUsers } from "../../data/MockUpListEmployees.json";
+import { useSelector } from "react-redux";
+import { formSlice } from "../../services/features/FormSlice";
 
+const Table = () => {
+  const user = useSelector((state) => state.formSlice.data);
+
+  return (
+    <div className="table">
+      <DataTable columns={dataColumn} data={user} />
+    </div>
+  );
+};
 export default Table;
