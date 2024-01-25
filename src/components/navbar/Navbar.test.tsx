60d560d182380fa22cb1878330db1393d54c9690
El Ghalbzouri Adnan <elghalbzouriadnan@gmail.com>
2024-01-25 09:46:01 +0100

    fix

--- src/components/navbar/Navbar.test.tsx
+++ src/components/navbar/Navbar.test.tsx
@@ -27,12 +27,19 @@ describe("Navbar Component", () => {
 
     const hamburger = screen.getByAltText("icône hamburger");
     expect(hamburger).toBeInTheDocument();
+    
+    const addUser = screen.queryByAltText("icône addUser");
+    expect(addUser).not.toBeInTheDocument();
 
-    const link = screen.getByText("Current");
-    expect(link).toBeInTheDocument();
+    const current = screen.getByText("Current");
+    expect(current).toBeInTheDocument();
+
+    const create = screen.queryByText("Create");
+    expect(create).not.toBeInTheDocument();
   });
 
-  it("navigates to the home page on clicking the logo", async () => {
+
+  test("navigates to the home page on clicking the logo", async () => {
     render(
       <Provider store={store}>
         <MemoryRouter>
@@ -46,7 +53,34 @@ describe("Navbar Component", () => {
     expect(window.location.pathname).toBe("/");
   });
 
-  // + rajouter toggle image hamburger + addUser :
+
+  test("toggles between hamburger and addUser on click", async () => {
+    userEvent.setup();
+
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <Navbar />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    const hamburgerActive = screen.getByAltText("icône hamburger");
+    expect(hamburgerActive).toBeInTheDocument();
+
+    const addUserInactive = screen.queryByAltText("icône addUser");
+    expect(addUserInactive).not.toBeInTheDocument();
+
+    await userEvent.click(hamburgerActive);
+    window.location.pathname = "/employees";
+
+    const addUserActive = screen.getByAltText("icône addUser");
+    expect(addUserActive).toBeInTheDocument();
+
+    const hamburgerInactive = screen.queryByAltText("icône hamburger");
+    expect(hamburgerInactive).not.toBeInTheDocument();
+  });
+
 
   test("toggles between Create and Current on link click", async () => {
     userEvent.setup();
@@ -59,13 +93,19 @@ describe("Navbar Component", () => {
       </Provider>
     );
 
-    const current = screen.getByText("Current");
-    expect(current).toBeInTheDocument();
+    const currentActive = screen.getByText("Current");
+    expect(currentActive).toBeInTheDocument();
 
-    await userEvent.click(current);
+    const createInactive = screen.queryByText("Create");
+    expect(createInactive).not.toBeInTheDocument();
+
+    await userEvent.click(currentActive);
     window.location.pathname = "/employees";
 
-    const create = screen.getByText("Create");
-    expect(create).toBeInTheDocument();
+    const createActive = screen.getByText("Create");
+    expect(createActive).toBeInTheDocument();
+
+    const currentInactive = screen.queryByText("Current");
+    expect(currentInactive).not.toBeInTheDocument();
   });
 });
