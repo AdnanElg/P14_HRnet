e1e7d166bbd31d9582b6b0e1c812ae02ce87adbf
El Ghalbzouri Adnan <elghalbzouriadnan@gmail.com>
2024-01-25 15:03:31 +0100

    fix

--- src/components/datepicker/Datepicker.test.tsx
+++ src/components/datepicker/Datepicker.test.tsx
@@ -3,14 +3,24 @@ import { render, screen } from "@testing-library/react";
 import store from "../../services/store";
 import { Provider } from "react-redux";
 import Datepicker from "./Datepicker";
-
+import { useForm } from "react-hook-form";
 
 // Test Integration :
 describe("Table Datepicker", () => {
+  const { control } = useForm();
+
   test("renders correctly", () => {
+    const props = {
+      label: "Date of Birth",
+      name: "dateofbirth",
+      error: "",
+      control: control,
+      resetKey: "",
+    }
+
     render(
       <Provider store={store}>
-        <Datepicker />
+        <Datepicker {...props}/>
       </Provider>
     );
 
--- src/components/dropdown/DropDown.test.tsx
+++ src/components/dropdown/DropDown.test.tsx
@@ -3,20 +3,37 @@ import { render, screen } from "@testing-library/react";
 import store from "../../services/store";
 import { Provider } from "react-redux";
 import DropDown from "./DropDown";
+import { useForm } from "react-hook-form";
 
+test("user selects an option", () => {
+  const { control } = useForm();
 
-// Test Integration :
-describe("Table Dropdown", () => {
-  test("renders correctly", () => {
-    render(
-      <Provider store={store}>
-        <DropDown />
-      </Provider>
-    );
+  const stateOptions = [
+    {
+      "name": "Alabama",
+      "abbreviation": "AL"
+    },
+    {
+      "name": "Alaska",
+      "abbreviation": "AK"
+    },
+  ];
 
-    const dropdown = screen.getByTestId("dropdown");
-    expect(dropdown).toBeInTheDocument();
-  });
+  const props = {
+    label: "State",
+    name: "state",
+    options: stateOptions,
+    error: "",
+    control: control,
+    resetKey: "",
+  };
+  
+  render(
+    <Provider store={store}>
+      <DropDown {...props}/>
+    </Provider>
+  );
+
+  const dropdown = screen.getByTestId("dropdown");
+  expect(dropdown).toBeInTheDocument();
 });
-
-// A revoir si autre test à faire ?
